c556bca89a12328c6a97863d9de6a4de250be88b
siskanuriaprilia <aprilias878@gmail.com>
2023-10-28 18:36:31 +0700

    modifikasi Linear

--- LinearSearch27.java
+++ LinearSearch27.java
@@ -1,18 +1,37 @@
+import java.util.Scanner;
+
 public class LinearSearch27 {
     public  static void main(String [] args) {
-      
-        int [] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
-        int hasil = 0;
+        Scanner input27 = new Scanner(System.in);
 
-        for (int i= 0; i < arrayInt.length; i++)
+        System.out.println("Masukkan jumlah elemen array : ");
+        int jumlahElemen = input27.nextInt();
+        int[] arrayInt = new int[jumlahElemen];
+
+        for (int i= 0; i < jumlahElemen; i++)
         {
-            if (arrayInt [i] == key)
-          {
-            hasil = i;
-            break;
-          }
-        } System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);
+        System.out.println("Masukkan elemen array ke-"+i+": ");
+        arrayInt[i] = input27.nextInt();
+        }
+        System.out.println("Masukkan key yang ingin dicari : ");
+        int key = input27.nextInt();
 
+        int hasil = -1;
+        for (int i = 0; i < jumlahElemen; i++)
+        {
+            if (arrayInt[i] == key)
+            {
+                hasil = i;
+                break;
+            }
+        }
+        if (hasil != -1)
+        {
+            System.out.println("Key ada diposisi index ke-" + hasil);
+        }
+        else
+        {
+            System.out.println("Key tidak ditemukan dalam array.");
+        }
     }
 }
