9b0c2caa6ebb46e2cd6c39357b50800646c6c89f
siskanuriaprilia <aprilias878@gmail.com>
2023-10-28 14:04:37 +0700

    modifikasi2

--- ArrayBilangan27.java
+++ ArrayBilangan27.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class ArrayBilangan27 {
 
     public  static void main(String [] args) {
-        
+
         int[] bil = {5,13,-7,17};
 
-        System.out.println(bil [0]);
-        System.out.println(bil [1]); 
-        System.out.println(bil [2]);
-        System.out.println(bil [3]);
+        for (int i = 0; i < 4; i++)
+        {
+            System.out.println(bil[i]);
+        }
     }
   }
 
