61fe07f381e5c6b12c854bb37406dba98f358d3b
siskanuriaprilia <aprilias878@gmail.com>
2023-10-28 15:33:25 +0700

    Array rata-rata siska

---
+++ ArrayRataNilai27.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ArrayRataNilai27 {
+
+    public  static void main(String [] args) {
+        Scanner input27 = new Scanner(System.in);
+
+        int[] nilaiMhs = new int [10];
+        double total = 0;
+        double rata_Rata;
+
+        for (int i = 0; i< nilaiMhs.length; i++)
+        {
+            System.out.println("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
+            nilaiMhs [i] = input27.nextInt();
+        }
+        {
+            for (int i = 0; i < nilaiMhs.length; i++)
+            {
+                total += nilaiMhs[i];
+            }
+        }
+        rata_Rata = total/nilaiMhs.length;
+        System.out.println("Rata-rata nilai = "+rata_Rata);
+    }
+}   
