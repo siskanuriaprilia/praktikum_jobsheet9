6725f37cead21405c04bca343366d2fbc23b847e
siskanuriaprilia <aprilias878@gmail.com>
2023-10-28 13:29:20 +0700

    salah update

--- ArrayNilai27.java
+++ ArrayNilai27.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class ArrayNilai27 {
 
     public  static void main(String [] args) {
-        
+        Scanner input27 = new Scanner(System.in);
+
         int [] nilaiAkhir = new int[10];
 
         for (int i = 0; i < nilaiAkhir.length; i++) 
